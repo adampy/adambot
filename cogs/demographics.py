a3d6ec562db05a434cdb5fc16b4383b521aaf31f
Adam Crawley <adamflint8@hotmail.co.uk>
2021-02-14 22:37:49 +0000

    Finished demographics chart

--- cogs/demographics.py
+++ cogs/demographics.py
@@ -1,9 +1,12 @@
 ﻿import discord
 from discord.ext import commands
-from .utils import Permissions, Todo
+from .utils import Permissions, Todo, send_file
 import asyncio
 import asyncpg
 from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+import matplotlib
 
 class Demographics(commands.Cog): # Tracks the change in specific roles. This works over multiple guilds, but cannot function like that due to utils.Permissions.
     def __init__(self, bot):
@@ -122,9 +125,32 @@ class Demographics(commands.Cog): # Tracks the change in specific roles. This wo
     @demographics.command(pass_context = True)
     @commands.has_any_role(*Permissions.STAFF)
     @commands.guild_only()
-    async def takesample(self, ctx, role: discord.Role):
-        """Adds a TODO saying that a sample is required ASAP."""
-        if role.id not in await self._get_roles(ctx.guild):
+    async def takesample(self, ctx, role: discord.Role = None):
+        """Adds a TODO saying that a sample is required ASAP. If `role` == None then all guild demographics are sampled."""
+        guild_tracked_roles = await self._get_roles(ctx.guild)
+        if role == None:
+            # Take a sample of all roles
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            question = await ctx.send(f"No role given, would you like to take a sample of all this guild's roles? (Type either 'yes' or 'no')")
+            try:
+                response = await self.bot.wait_for("message", check = check, timeout = 300) # 5 minute timeout
+            except asyncio.TimeoutError:
+                await question.edit(content = "Demographic command timed out. :sob:")
+                return
+
+            if response.content.lower() == "yes":
+                for role in guild_tracked_roles:
+                    await self._require_sample(ctx.guild.get_role(role))
+                await ctx.send("All roles sampled! :ok_hand:")
+            elif response.content.lower() == "no":
+                await ctx.send("Operation cancelled.")
+            else:
+                await ctx.send("Unknown response - operation cancelled.")
+            return # return here means return does not need to be placed inside each condition
+
+        if role.id not in await guild_tracked_roles:
             await ctx.send("This role is not currently being tracked!")
             return
 
@@ -163,13 +189,40 @@ class Demographics(commands.Cog): # Tracks the change in specific roles. This wo
             numbers.append(len([x for x in ctx.guild.members if role in [y.name for y in x.roles]]))
         message = """We have {} members.
         
-{} Post-GCSE
-{} Year 11s
-{} Year 10s
-{} Year 9s
+**{}** Post-GCSE
+**{}** Year 11s
+**{}** Year 10s
+**{}** Year 9s
 *The demographics system is currently undergoing maintenance, and there will soon be new features added.*""".format(*numbers)
         await ctx.send(message)
 
+    @demographics.command(pass_context = True)
+    @commands.has_any_role(*Permissions.MEMBERS)
+    @commands.guild_only()
+    async def chart(self, ctx):
+        """View a guild's demographics over time"""
+        fig, ax = plt.subplots()
+        vals = [[0,0]]
+        #ax.plot([x[0] for x in vals], [x[1] for x in vals], 'b-o', linewidth=0.5, markersize=1)
+
+        async with self.bot.pool.acquire() as connection:
+            role_data = await connection.fetch("SELECT role_id, id FROM demographic_roles WHERE guild_id = $1", ctx.guild.id)
+            
+            for role in role_data:
+                data = await connection.fetch("SELECT taken_at, n FROM demographic_samples WHERE role_reference = $1", role[1])
+                role = ctx.guild.get_role(role[0])
+                rgb_scaled_tuple = tuple(x/255 for x in role.color.to_rgb()) # Scale 0-255 integers down to 0-1 floats
+
+                ax.plot([x[0] for x in data], [x[1] for x in data], 'b-o', linewidth = 1, markersize = 2, color = rgb_scaled_tuple, label = role.name)
+
+        ax.set(xlabel='Time', ylabel='Frequency', title=f"{ctx.guild.name}'s  demographics")
+        ax.grid()
+        ax.legend(loc = "upper left")
+        ax.set_ylim(bottom = 0)
+        ax.fmt_xdata = DateFormatter('% Y-% m-% d % H:% M:% S') 
+        fig.autofmt_xdate()
+
+        await send_file(fig, ctx.channel, "demographics-data")
 
 def setup(bot):
     bot.add_cog(Demographics(bot))
