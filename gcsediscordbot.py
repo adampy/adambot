afe7f5e71910c2ea432f9b4dcd59cde491c17c26
monkeyboy2805 <monkeyboy2805@gmail.com>
2021-02-14 01:18:32 +0000

    I'm a plantpot, ignore last commit

--- gcsediscordbot.py
+++ gcsediscordbot.py
@@ -49,7 +49,7 @@ class AdamBot(Bot):
         self.load_cogs()
         self.loop.create_task(self.execute_todos())
         self.pool : asyncpg.pool.Pool = self.loop.run_until_complete(asyncpg.create_pool(self.DB + "?sslmode=require", max_size=20))
-        self.last_active = []  # easiest to put here for now, may move to a cog later
+        self.last_active = {} # easiest to put here for now, may move to a cog later
         self.run(os.environ.get('TOKEN'))
 
     def load_cogs(self):
