f2c365f49fc43290096fa54bb294bd9f4fff5afe
Adam Crawley <adamflint8@hotmail.co.uk>
2021-07-03 21:42:22 +0100

    Added thumbnail option to the embeds

--- cogs/utils.py
+++ cogs/utils.py
@@ -412,23 +412,27 @@ INFORMATION_BLUE = Colour.from_rgb(32, 141, 177)
 
 # EMBED RESPONSES
 class DefaultEmbedResponses:
-    async def invalid_perms(bot, ctx):
+    async def invalid_perms(bot, ctx, thumbnail_url = ""):
         """Internal procedure that is executed when a user has invalid perms"""
         embed = Embed(title = f':x: You do not have permissions to do that!', description = "You are but a weakling.", color = ERROR_RED)
         embed.set_footer(text = f"Requested by: {ctx.author.display_name} ({ctx.author})\n" + bot.correct_time().strftime(bot.ts_format), icon_url = ctx.author.avatar_url)
+        if thumbnail_url: embed.set_thumbnail(url = thumbnail_url)
         await ctx.reply(embed = embed)
 
-    async def error_embed(bot, ctx, title, desc = ""):
+    async def error_embed(bot, ctx, title, desc = "", thumbnail_url = ""):
         embed = Embed(title = f':x: {title}', description = desc, color = ERROR_RED)
         embed.set_footer(text = f"Requested by: {ctx.author.display_name} ({ctx.author})\n" + bot.correct_time().strftime(bot.ts_format), icon_url = ctx.author.avatar_url)
+        if thumbnail_url: embed.set_thumbnail(url = thumbnail_url)
         await ctx.reply(embed = embed)
 
-    async def success_embed(bot, ctx, title, desc = ""):
+    async def success_embed(bot, ctx, title, desc = "", thumbnail_url = ""):
         embed = Embed(title = f':white_check_mark: {title}', description = desc, color = SUCCESS_GREEN)
         embed.set_footer(text = f"Requested by: {ctx.author.display_name} ({ctx.author})\n" + bot.correct_time().strftime(bot.ts_format), icon_url = ctx.author.avatar_url)
+        if thumbnail_url: embed.set_thumbnail(url = thumbnail_url)
         await ctx.reply(embed = embed)
 
-    async def information_embed(bot, ctx, title, desc = ""):
+    async def information_embed(bot, ctx, title, desc = "", thumbnail_url = ""):
         embed = Embed(title = f':information_source: {title}', description = desc, color = INFORMATION_BLUE)
         embed.set_footer(text = f"Requested by: {ctx.author.display_name} ({ctx.author})\n" + bot.correct_time().strftime(bot.ts_format), icon_url = ctx.author.avatar_url)
+        if thumbnail_url: embed.set_thumbnail(url = thumbnail_url)
         await ctx.reply(embed = embed)
