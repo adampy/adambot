59379af932642d8d8820453dca5cf8410e0f7932
monkeyboy2805 <monkeyboy2805@gmail.com>
2021-09-16 02:51:33 +0100

    Always has to be one that gets missed...

--- libs/misc/decorators.py
+++ libs/misc/decorators.py
@@ -19,7 +19,7 @@ def is_staff(func):
         while not cog.bot.online:
             await asyncio.sleep(1)  # Wait else DB won't be available
 
-        staff_role_id = cog.bot.get_config_key(ctx, "staff_role")
+        staff_role_id = await cog.bot.get_config_key(ctx, "staff_role")
         if staff_role_id in [y.id for y in ctx.author.roles] or ctx.author.guild_permissions.administrator:
             return await func(cog, ctx, *args, **kwargs)
         else:
