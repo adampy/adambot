fe31252b05383b69bfeee872dc3048fa86ffded7
monkeyboy2805 <monkeyboy2805@gmail.com>
2021-11-23 18:33:57 +0000

    pov: I just discovered I am an idiot

--- scripts/tests/test_reqs.py
+++ scripts/tests/test_reqs.py
@@ -6,6 +6,7 @@ except ImportError:  # is part of setuptools
     import subprocess
     try:
         subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--force-reinstall', 'setuptools', '--user'])
+        import pkg_resources
     except Exception as e:
         print(f"Something has gone horribly wrong and the dependencies cannot be tested\n{type(e).__name__}: {e}")
         exit(1)
